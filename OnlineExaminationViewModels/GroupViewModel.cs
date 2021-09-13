035500c945e016044572d7691bb9ca76073c75a4
Mete <metekaba96@gmail.com>
2021-09-14 00:25:25 +0300

    GroupViewModel is developed.

---
+++ OnlineExaminationViewModels/GroupViewModel.cs
@@ -0,0 +1,41 @@
+﻿using OnlineExaminationDAL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineExaminationViewModels
+{
+    public class GroupViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        [Display(Name="Group Name")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+        public int UsersId { get; set; }
+        public List<GroupViewModel> GroupList { get; set; }
+        public int TotalCount { get; set; }
+        public GroupViewModel(Groups group)
+        {
+            Id = group.Id;
+            Name = group.Name ?? "";
+            Description = group.Description ?? "";
+            UsersId = group.UsersId;
+        }
+        public Groups ConvertGroupsViewModel(GroupViewModel groupViewModel)
+        {
+            return new Groups
+            {
+                Id = groupViewModel.Id,
+                Name = groupViewModel.Name ?? "",
+                Description = groupViewModel.Description ?? "",
+                UsersId = groupViewModel.UsersId
+            };
+        }
+    }
+}
