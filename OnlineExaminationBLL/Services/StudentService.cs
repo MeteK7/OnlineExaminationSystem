5787fc07214af93fea573899dba17c6236ebbffd
Mete <metekaba96@gmail.com>
2021-09-15 01:31:35 +0300

    Create StudentService.cs

---
+++ OnlineExaminationBLL/Services/StudentService.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Extensions.Logging;
+using OnlineExaminationDAL;
+using OnlineExaminationDAL.UnitOfWork;
+using OnlineExaminationViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineExaminationBLL.Services
+{
+    public class StudentService : IStudentService
+    {
+        IUnitOfWork _unitOfWork;
+        ILogger<StudentService> _iLogger;
+
+        public StudentService(IUnitOfWork unitOfWork, ILogger<StudentService> iLogger)
+        {
+            _unitOfWork = unitOfWork;
+            _iLogger = iLogger;
+        }
+
+        public Task<StudentViewModel> AddAsync(StudentViewModel studentViewModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PagedResult<StudentViewModel> GetAll(int pageNumber, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Students> GetAllStudents()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ResultViewModel> GetExamResults(int studentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public StudentViewModel GetStudentDetails(int studentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SetExamResult(AttendExamViewModel attendExamViewModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SetGroupIdToStudents(GroupViewModel groupViewModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<StudentViewModel> UpdateAsync(StudentViewModel studentViewModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
