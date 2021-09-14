9706aa62e680f7b3fba832ead0fedd5a9c4a8a9c
Mete <metekaba96@gmail.com>
2021-09-15 01:15:38 +0300

    The interface named IAccountService is created.

---
+++ OnlineExaminationBLL/Services/IAccountService.cs
@@ -0,0 +1,16 @@
+﻿using OnlineExaminationViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineExaminationBLL.Services
+{
+    public interface IAccountService
+    {
+        LoginViewModel Login(LoginViewModel loginViewModel);
+        bool AddTeacher(UserViewModel userViewModel);
+        PagedResult<UserViewModel> GetAllTeacher(int pageNumber, int pageSize);
+    }
+}
