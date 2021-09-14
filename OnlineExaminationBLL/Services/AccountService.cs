4f90d82885e189c215aaee26b7d49af7b461e360
Mete <metekaba96@gmail.com>
2021-09-15 01:26:01 +0300

    IStudentService is created.

---
+++ OnlineExaminationBLL/Services/AccountService.cs
@@ -0,0 +1,33 @@
+﻿using OnlineExaminationDAL.UnitOfWork;
+using OnlineExaminationViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineExaminationBLL.Services
+{
+    public class AccountService : IAccountService
+    {
+        IUnitOfWork _unitOfWork;
+        public AccountService()
+        {
+
+        }
+        public bool AddTeacher(UserViewModel userViewModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PagedResult<UserViewModel> GetAllTeacher(int pageNumber, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public LoginViewModel Login(LoginViewModel loginViewModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ OnlineExaminationBLL/Services/IStudentService.cs
@@ -0,0 +1,22 @@
+﻿using OnlineExaminationDAL;
+using OnlineExaminationViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineExaminationBLL.Services
+{
+    public interface IStudentService
+    {
+        PagedResult<StudentViewModel> GetAll(int pageNumber, int pageSize);
+        Task<StudentViewModel> AddAsync(StudentViewModel studentViewModel);
+        IEnumerable<Students> GetAllStudents();
+        bool SetGroupIdToStudents(GroupViewModel groupViewModel);
+        bool SetExamResult(AttendExamViewModel attendExamViewModel);
+        IEnumerable<ResultViewModel> GetExamResults(int studentId);
+        StudentViewModel GetStudentDetails(int studentId);
+        Task<StudentViewModel> UpdateAsync(StudentViewModel studentViewModel);
+    }
+}
