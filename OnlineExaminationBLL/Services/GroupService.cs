e20d4857412f81e06809e2deca4b8d060f884fac
Mete <metekaba96@gmail.com>
2021-09-29 01:09:10 +0300

    GetAllGroups and GroupListInfo methods are composed.

--- OnlineExaminationBLL/Services/GroupService.cs
+++ OnlineExaminationBLL/Services/GroupService.cs
@@ -38,7 +38,40 @@ namespace OnlineExaminationBLL.Services
 
         public PagedResult<GroupViewModel> GetAllGroups(int pageNumber, int pageSize)
         {
-            throw new NotImplementedException();
+            var model = new GroupViewModel();
+
+            try
+            {
+                int ExcludeRecords = (pageSize * pageNumber) - pageSize;
+                List<GroupViewModel> detailList = new List<GroupViewModel>();
+                var modelList = _unitOfWork.GenericRepository<Groups>().GetAll().Skip(ExcludeRecords).Take(pageSize).ToList();
+                var totalCount = _unitOfWork.GenericRepository<Groups>().GetAll().ToList();
+                detailList = GroupListInfo(modelList);
+                if (detailList!=null)
+                {
+                    model.GroupList = detailList;
+                    model.TotalCount = totalCount.Count();
+                }
+            }
+            catch (Exception ex)
+            {
+                _iLogger.LogError(ex.Message);
+                throw;
+            }
+
+            var result = new PagedResult<GroupViewModel>
+            {
+                Data = model.GroupList,
+                TotalItems = model.TotalCount,
+                PageNumber = pageNumber,
+                PageSize = pageSize
+            };
+            return result;
+        }
+
+        private List<GroupViewModel> GroupListInfo(List<Groups> modelList)
+        {
+            return modelList.Select(o => new GroupViewModel(o)).ToList();
         }
 
         public IEnumerable<Groups> GetAllGroups()
