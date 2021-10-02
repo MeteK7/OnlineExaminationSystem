b46441c02d962f3adec3a3654beda5bbb0fb87d2
Mete <metekaba96@gmail.com>
2021-10-02 14:27:14 +0300

    IExamService is created.

---
+++ OnlineExaminationBLL/Services/IExamService.cs
@@ -0,0 +1,17 @@
+﻿using OnlineExaminationDAL;
+using OnlineExaminationViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineExaminationBLL.Services
+{
+    public interface IExamService
+    {
+        PagedResult<ExamViewModel> GetAll(int pageNumber, int pageSize);
+        Task<ExamViewModel> AddAsync(ExamViewModel examVM);
+        IEnumerable<Exams> GetAllExams();
+    }
+}
