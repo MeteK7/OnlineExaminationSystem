733ba54cc617f50bff1f2b29e1a8fdffb8b3fb46
Mete <metekaba96@gmail.com>
2021-10-02 14:36:04 +0300

    AddAsync method is composed.

---
+++ OnlineExaminationBLL/Services/ExamService.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Logging;
+using OnlineExaminationDAL;
+using OnlineExaminationDAL.UnitOfWork;
+using OnlineExaminationViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineExaminationBLL.Services
+{
+    public class ExamService : IExamService
+    {
+        IUnitOfWork _unitOfWork;
+        ILogger<ExamService> _iLogger;
+
+        public ExamService(IUnitOfWork unitOfWork, ILogger<ExamService> iLogger)
+        {
+            _unitOfWork = unitOfWork;
+            _iLogger = iLogger;
+        }
+
+        public async Task<ExamViewModel> AddAsync(ExamViewModel examVM)
+        {
+            try
+            {
+                Exams objExam = examVM.ConvertViewModel(examVM);
+                await _unitOfWork.GenericRepository<Exams>().AddAsync(objExam);
+                _unitOfWork.Save();
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+            return examVM;
+        }
+
+        public PagedResult<ExamViewModel> GetAll(int pageNumber, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Exams> GetAllExams()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
