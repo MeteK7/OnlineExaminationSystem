5fb6f25d4472defd1c121d452336c54f464ec76a
Mete <metekaba96@gmail.com>
2021-10-08 23:27:01 +0300

    AccountController is created.

---
+++ OnlineExaminationWeb/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineExaminationBLL.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineExaminationWeb.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IAccountService _accountService;
+
+        public AccountController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
