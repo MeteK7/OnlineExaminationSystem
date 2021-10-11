43ed153b456bf74c23502b85fc088fe70b5b2160
Mete <metekaba96@gmail.com>
2021-10-11 22:24:19 +0300

    UsersController is developed.

---
+++ OnlineExaminationWeb/Controllers/UsersController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineExaminationBLL.Services;
+using OnlineExaminationViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineExaminationWeb.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly IAccountService _accountService;
+
+        public UsersController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        public IActionResult Index(int pageNumber = 1, int pageSize = 10)
+        {
+            return View(_accountService.GetAllTeachers(pageNumber,pageSize));
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(UserViewModel userViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _accountService.AddTeacher(userViewModel);
+                return RedirectToAction("Index");
+            }
+            return View(userViewModel);
+        }
+    }
+}
