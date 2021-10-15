0974c4942ab5ab8b0caa47b309a5105f5b610a2c
Mete <metekaba96@gmail.com>
2021-10-15 06:43:19 +0300

    GroupsController is created.

---
+++ OnlineExaminationWeb/Controllers/GroupsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineExaminationBLL.Services;
+using OnlineExaminationViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineExaminationWeb.Controllers
+{
+    public class GroupsController : Controller
+    {
+        private readonly IGroupService _groupService;
+        private readonly IStudentService _studentService;
+
+        public GroupsController(IGroupService groupService, IStudentService studentService)
+        {
+            _groupService = groupService;
+            _studentService = studentService;
+        }
+
+        public IActionResult Index(int pageNumber=1,int pageSize=10)
+        {
+            return View(_groupService.GetAllGroups(pageNumber,pageSize));
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Create(GroupViewModel groupViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                groupViewModel.UsersId = 1;
+                await _groupService.AddGroupAsync(groupViewModel);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(groupViewModel);
+        }
+    }
+}
